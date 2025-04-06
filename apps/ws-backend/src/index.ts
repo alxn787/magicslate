a7ec8b7d2678a1fc317e9397eaff85c6f35fc0d7
alxn787 <alenabraham787@gmail.com>
2025-04-06 16:36:25 +0530

    added stuff

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,12 +1,24 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection",function connection(ws) {
+wss.on("connection",function connection(ws,request) {
+
+    const url = request.url;
+    const params = new URLSearchParams(url?.split("?")[1]);
+    const token = params.get("token") || "";
+    const decoded = jwt.verify(token,process.env.JWT_SECRET as string);
+
+    if(!(decoded as JwtPayload).userId || !decoded){
+        ws.close();
+        return;
+    }
+
     ws.on("error",console.error);
 
     ws.on("message",function message(data){
-        console.log(data);
+        ws.send("Hello World!");
     })
-    ws.send("Hello World!");
+
 });
